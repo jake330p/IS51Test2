ed5c6e009da1a815f1d709ac533584e193951bd1
Jacob Puett <jake330@mail.fresnostate.edu>
2020-12-03 17:02:45 -0800

    finished structured english

---
+++ test_2.py
@@ -0,0 +1,9 @@
+"""
+The program is being used to display information about students grades
+Specifically it is being used to display the number of grades, the average grade, and the 
+percentage of grades that are above the average
+The program will draw the grades from the file and identify how many grades are listed
+Then, it will sum the grades listed in the file and return the average grade 
+Finally, it will determine all of the grades in the list that are above the average grade 
+and return the percentage
+"""
